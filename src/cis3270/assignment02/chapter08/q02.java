fe61eb0388aaacac99d763f55b99087e4894a3aa
evelynngo14 <evelynngo14@gmail.com>
2025-10-28 15:30:19 -0400

    Begin q04

--- src/cis3270/assignment02/chapter08/q02.java
+++
@@ -1,7 +0,0 @@
-package cis3270.assignment02.chapter08;
-
-public class q02 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/cis3270/assignment02/chapter08/q04.java
@@ -0,0 +1,16 @@
+package cis3270.assignment02.chapter08;
+//(Compute the weekly hours for each employee) Suppose the weekly hours for all
+//employees are stored in a two-dimensional array. Each row records an employ-
+//ee’s seven-day work hours with seven columns. For example, the following
+//array stores the work hours for eight employees. Write a program that displays
+//employees and their total hours in decreasing order of the total hours
+
+public class q04 {
+    public static void main(String[] args) {
+
+    }
+    public static double sumMajorDiagonal(double[][]m) {
+        
+    }
+
+}
